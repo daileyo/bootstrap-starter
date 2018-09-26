7eddb373c7f413d4f8cf4dd2f5e9d8189db01826
daileyo <dale13@gmail.com>
2018-09-26 08:39:43 -0700

    Added additional formatting and content to starter page. Updated project to copy required files to appropriate directories. Added npm lock, and ability tro run page from npm.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var express = require('express');
 var app = express();
 
-app.use('/css', express.static(__dirname + 'node_modules/bootstrap/dist/css/bootstrap.css'));
+app.use('./css', express.static(__dirname + 'node_modules/bootstrap/dist/css/bootstrap.css'));
+app.use('./js', express.static(__dirname + 'node_modules/bootstrap/dist/js/bootstrap.js'));
+app.use('./js', express.static(__dirname + 'node_modules/popper.js/dist/popper.css'));
+app.use('./js', express.static(__dirname + 'node_modules/jquery/dist/jquery.js'));
