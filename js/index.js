c53d1cbcfcc79c235e4048029ad561f28ffdab0c
daileyo <dale13@gmail.com>
2019-07-15 23:16:50 -0700

    Cleaned up main. -  removed uneeded css -  removed uneeded js -  adjusted color scheme

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-    $('.header').height($(window).height());
-    $('.about').height($(window).height());
-    $('.projects').height($(window).height());
     $('.navbar a').click(function(){
         $('body,html').animate({
             scrollTop:$("#" + $(this).data('value')).offset().top
         },1000)
     });
+    $(window).on("resize", function(){
+        $('.page').height($(document).innerHeight());
+    }).resize();
 })
