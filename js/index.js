4c211d24bcb5096e69d184fb668fb2ffb905d5f4
Robert Dailey <Robert.Dailey@exlservice.com>
2019-07-16 12:55:44 -0700

    Added Projects Cleaned up styling. Improved responsiveness to page resizing.

--- js/index.js
+++ js/index.js
@@ -4,8 +4,9 @@ $(document).ready(function(){
             scrollTop:$("#" + $(this).data('value')).offset().top
         },1000)
     });
-    $("#debug-page-size").text("Window:  " + $(window).height() + "  Document (inner):  " + $(document).height());
     $(window).on("resize", function(){
-        $(".page").height($(document).innerHeight());
+        $(".page").css("min-height", $(window).innerHeight());
     }).resize();
+    /*debug*/
+    $("#debug-page-size").text("Window:  " + $(window).height() + "  Document (inner):  " + $(document).height());
 })
